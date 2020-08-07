763a3793c3f1bd40898e6e2590e17180deaeb24a
Confidence Ugolo <ugoloconfidence@gmail.com>
2020-08-07 12:26:02 +0100

    Made adjustments to the app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -35,25 +35,29 @@ class AppServiceProvider extends ServiceProvider
         \DB::connection()->getPdo();
         view()->composer('*',function($view){
 
-            if(\DB::connection()->getDatabaseName()){
-                if(Schema::hasTable('settings')){
-                    $general_settings = Setting::first();
-                    // if(empty($general_settings)){
-                    //     $general_settings = Setting::create([
-                    //         'min_investment' => 25,
-                    //         'min_withdrawal' => 10,
-                    //         'withdrawal_rate' => 0.05,
-                    //         'transfer_rate' => 0.02,
-                    //         'fifteen_percent_min' => 75,
-                    //     ]);
-                    // }
-                    $view->with([
-                        "general_settings" => $general_settings ,
-                        'web_source' => env('ASSET_URL').'/web',
-                        'admin_source' => env('ASSET_URL').'/'."dashboard",
-                    ]);
-                }
-            }
+            // if(\DB::connection()->getDatabaseName()){
+            //     // if(Schema::hasTable('settings')){
+            //     //     $general_settings = Setting::first();
+            //         // if(empty($general_settings)){
+            //         //     $general_settings = Setting::create([
+            //         //         'min_investment' => 25,
+            //         //         'min_withdrawal' => 10,
+            //         //         'withdrawal_rate' => 0.05,
+            //         //         'transfer_rate' => 0.02,
+            //         //         'fifteen_percent_min' => 75,
+            //         //     ]);
+            //         // }
+            //         $view->with([
+            //             // "general_settings" => $general_settings ,
+            //             'web_source' => env('ASSET_URL').'/web',
+            //             'admin_source' => env('ASSET_URL').'/'."dashboard",
+            //         ]);
+            //     // }
+            // }
+            $view->with([
+                'web_source' => env('ASSET_URL').'/web',
+                'admin_source' => env('ASSET_URL').'/'."dashboard",
+            ]);
         });
 
     }
