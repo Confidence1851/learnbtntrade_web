8f8edcf361cdaaeafbd7ad395b567c4e41baede5
Confidence Ugolo <ugoloconfidence@gmail.com>
2020-10-17 10:52:23 +0100

    Configured images

--- resources/views/web/fragments/home_slider.blade.php
+++ resources/views/web/fragments/home_slider.blade.php
@@ -1,6 +1,13 @@
 <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel" >
     <div class="carousel-inner">
       <div class="carousel-item active" data-interval="10000">
+        <div class="slider_object" style="background-image: url({{ $web_source }}/images/main-slider/slider1.jpg)">
+            <div class="slider_btn_area">
+                <a href="{{ route('about_us') }}" class="btn btn-primary slider_btn">About Us</a>
+            </div>
+        </div>
+      </div>
+      <div class="carousel-item" data-interval="10000">
         <div class="slider_object" style="background-image: url({{ $web_source }}/images/main-slider/slider2.jpeg)">
             <div class="slider_btn_area">
                 <a href="{{ route('about_us') }}" class="btn btn-primary slider_btn">About Us</a>
@@ -16,13 +23,6 @@
       </div>
       <div class="carousel-item" data-interval="10000">
         <div class="slider_object" style="background-image: url({{ $web_source }}/images/main-slider/slider3.jpeg)">
-            <div class="slider_btn_area">
-                <a href="{{ route('about_us') }}" class="btn btn-primary slider_btn">About Us</a>
-            </div>
-        </div>
-      </div>
-      <div class="carousel-item" data-interval="10000">
-        <div class="slider_object" style="background-image: url({{ $web_source }}/images/main-slider/slider1.png)">
             <div class="slider_btn_area">
                 <a href="{{ route('about_us') }}" class="btn btn-primary slider_btn">About Us</a>
             </div>
