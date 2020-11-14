4df2ef99f0b465c20c48a737d7d07d244ed98fe1
Confidence Ugolo <ugoloconfidence@gmail.com>
2020-11-14 19:09:45 +0100

    Banners arranged

--- resources/views/web/fragments/home_slider.blade.php
+++ resources/views/web/fragments/home_slider.blade.php
@@ -1,6 +1,13 @@
 <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel" >
     <div class="carousel-inner">
       <div class="carousel-item active" data-interval="10000">
+        <div class="slider_object" style="background-image: url({{ $web_source }}/images/main-slider/slider1_.jpg)">
+            <div class="slider_btn_area">
+                <a href="{{ route('about_us') }}" class="btn btn-primary slider_btn">About Us</a>
+            </div>
+        </div>
+      </div>
+      <div class="carousel-item " data-interval="10000">
         <div class="slider_object" style="background-image: url({{ $web_source }}/images/main-slider/slider1.jpeg)">
             <div class="slider_btn_area">
                 <a href="{{ route('about_us') }}" class="btn btn-primary slider_btn">About Us</a>
@@ -15,7 +22,7 @@
         </div>
       </div>
       <div class="carousel-item" data-interval="10000">
-        <div class="slider_object" style="background-image: url({{ $web_source }}/images/main-slider/slider4.jpeg)">
+        <div class="slider_object" style="background-image: url({{ $web_source }}/images/main-slider/slider2_.jpeg)">
             {{-- <div class="slider_btn_area">
                 <a href="{{ route('about_us') }}" class="btn btn-primary slider_btn">About Us</a>
             </div> --}}
@@ -28,13 +35,7 @@
             </div>
         </div>
       </div>
-      <div class="carousel-item" data-interval="10000">
-        <div class="slider_object" style="background-image: url({{ $web_source }}/images/main-slider/slider5.jpeg)">
-            <div class="slider_btn_area">
-                <a href="{{ route('about_us') }}" class="btn btn-primary slider_btn">About Us</a>
-            </div>
-        </div>
-      </div>
+     
     </div>
     <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
