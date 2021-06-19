ca13df50001ef423588de20c0b91ebe4878f8cc8
Confidence Ugolo <ugoloconfidence@gmail.com>
2021-06-19 18:59:16 +0100

    Testimonials changes implemented

--- resources/views/web/fragments/testimonial_item.blade.php
+++ resources/views/web/fragments/testimonial_item.blade.php
@@ -1,11 +1,20 @@
 <div class="item">
     <div class="testimonial-15 quote-right">
         <div class="testimonial-text">
-            <p>{!! $testimonial->content !!}</p>
-        </div>
-        <div class="testimonial-detail clearfix">
-            <div class="testimonial-pic radius"><img src="{{$testimonial->getAvatar()}}" width="100" height="100" alt=""></div>
-            <strong class="testimonial-name">{{ $testimonial->name }}</strong> <span class="testimonial-position">{{ $testimonial->title }}</span>
+            @if (strtolower($testimonial->content_type) == 'text')
+                <p>{!! $testimonial->content !!}</p>
+            @else
+                @php($image = $testimonial->getContentImage())
+                    <a href="{{ $image }}" target="_blank">
+                        <img src="{{ $image }}" height="100" alt="Avatar" />
+                    </a>
+                @endif
+            </div>
+            <div class="testimonial-detail clearfix">
+                <div class="testimonial-pic radius"><img src="{{ $testimonial->getAvatar() }}" width="100" height="100"
+                        alt=""></div>
+                <strong class="testimonial-name">{{ $testimonial->name }}</strong> <span
+                    class="testimonial-position">{{ $testimonial->title }}</span>
+            </div>
         </div>
     </div>
-</div>
